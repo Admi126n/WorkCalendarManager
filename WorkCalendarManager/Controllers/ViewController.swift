686e49f407e39aca968f9aa5c7f22a138b4b97f6
Adam Tokarski <a.tokarski126@gmail.com>
2023-03-30 12:42:48 +0200

    add a few things which can help with getting events from calendar

--- WorkCalendarManager/Controllers/ViewController.swift
+++ WorkCalendarManager/Controllers/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import EventKit
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func test(_ sender: UIButton) {
+        print(Date().startOfMonth())
+        print(Date().endOfMonth())
+        
+        // Create event store object
+        let store = EKEventStore()
+        
+        // Request access to calendar
+        store.requestAccess(to: .event) { granted, error in
+            if error != nil {
+                print(error!)
+            }
+        }
+        
+        let calendars = store.calendars(for: .event)
+        for calendar in calendars {
+            if calendar.title == "Praca" {
+                
+                let oneMonthAgo = Date(timeIntervalSinceNow: -30*24*3600)
+                let oneMonthAfter = Date(timeIntervalSinceNow: 30*24*3600)
+                let predicate =  store.predicateForEvents(withStart: oneMonthAgo, end: oneMonthAfter, calendars: [calendar])
+                
+                let events = store.events(matching: predicate)
+                
+                for event in events {
+                    print(event.title!)
+                    let start = event.startDate
+                    let end = event.endDate
+                    let x = end!.timeIntervalSinceReferenceDate - start!.timeIntervalSinceReferenceDate
+                    print(x) // difference in seconds
+                }
+            }
+        }
+    }
+    
 }
 
+extension Date {
+    func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
+    }
+    
+    func endOfMonth() -> Date {
+        return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth())!
+    }
+}
